768fcb96f5e158362e16b4f406882160811e0205
Vicky Stephens <vicky.k.stephens@gmail.com>
2017-11-06 00:18:58 +0000

    tidy up a bit

--- collect.py
+++ collect.py
@@ -1,39 +1,33 @@
 import tweepy
 import json
-
 import matplotlib.pyplot as plt
 import matplotlib.dates as dates
-import numpy as np
 
-import csv
-
-keysfile = open('keys.json', 'r')
-keys = json.load(keysfile)['twitter']
-
-# json loaded data as unicode
-consumer_key = str(keys['consumer_key'])
-consumer_secret = str(keys['consumer_secret'])
-access_key = str(keys['access_token'])
-access_secret = str(keys['access_secret'])
-
+def load_keys():
+    keysfile = open('keys.json', 'r')
+    keys = json.load(keysfile)['twitter']
+    # json loaded data as unicode, convert as str in case
+    consumer_key = str(keys['consumer_key'])
+    consumer_secret = str(keys['consumer_secret'])
+    access_key = str(keys['access_token'])
+    access_secret = str(keys['access_secret'])
+    return consumer_key, consumer_secret, access_key, access_secret
 
 def initialize_twitter_api():
+    consumer_key, consumer_secret, access_key, access_secret = load_keys()
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_key, access_secret)
     api = tweepy.API(auth)
     return api
-
 
 def get_all_tweets(screenname):
     api = initialize_twitter_api()
     tweets = api.user_timeline(screen_name = screenname,count=200)
     return tweets
 
-
 def get_tweet_times(tweets):
     tweet_times = [ tweet.created_at for tweet in tweets]
     return tweet_times
-
 
 def plot_activity(tweet_time):
     ax = plt.subplot(111)
